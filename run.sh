af327eac149b6d8666599bb3a79ecee4cd3c0942
Carl Smedstad <carl.smedstad@protonmail.com>
2021-05-16 22:25:18 +0200

    Fix shellcheck warnings
    Opted to break up the one-liner that installed the necessary
    dependencies. While not as terse, I think this way is a bit easier to
    follow, and it passes shellcheck.

--- run.sh
+++ run.sh
@@ -8,14 +8,17 @@ cd /tmp/pkg
 
 # Install (official repo + AUR) dependencies using yay. We avoid using makepkg
 # -s since it is unable to install AUR dependencies.
-yay -Sy --noconfirm \
-    $(pacman --deptest $(source ./PKGBUILD && echo ${depends[@]} ${makedepends[@]}))
+depends=(); makedepends=()
+# shellcheck disable=1091
+. ./PKGBUILD
+deps=( "${depends[@]}" "${makedepends[@]}" )
+pacman --deptest "${deps[@]}" | xargs yay -Sy --noconfirm
 
 # Do the actual building
 makepkg -f
 
 # Store the built package(s). Ensure permissions match the original PKGBUILD.
 if [ -n "$EXPORT_PKG" ]; then
-    sudo chown $(stat -c '%u:%g' /pkg/PKGBUILD) ./*pkg.tar*
+    sudo chown "$(stat -c '%u:%g' /pkg/PKGBUILD)" ./*pkg.tar*
     sudo mv ./*pkg.tar* /pkg
 fi
